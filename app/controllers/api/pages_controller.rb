1603c56fa7e5ea2d1bb085ef69a04d352a5f8133
case2912 <shiroyama@maia.eonet.ne.jp>
2019-05-23 14:48:46 +0900

    modify show and next method in pages_controller

--- app/controllers/api/pages_controller.rb
+++ app/controllers/api/pages_controller.rb
@@ -2,9 +2,18 @@ module Api
   class PagesController < ApplicationController
     def show
       page = Page.where(story_id:params[:story_id]).find(params[:id])
-      render json: page
+      children = Page.where(story_id:params[:story_id]).where(parent_id:params[:id])
+      @json = page.attributes
+      @json.store('children', children)
+      render json: @json
     end
     def next
+      story_id = params[:story_id]
+      parent_id = params[:page_id]
+      text = params[:text]
+      name = params[:name]
+      page = Page.create(name:name, text:text, story_id:story_id,parent_id:parent_id)
+      render json: page
     end
   end
 end
