dc2176ce83d4d49271dfb415cf6c968172ef4324
case2912 <shiroyama@maia.eonet.ne.jp>
2019-05-23 13:03:13 +0900

    modify stories_controller

--- app/controllers/api/stories_controller.rb
+++ app/controllers/api/stories_controller.rb
@@ -5,9 +5,10 @@ module Api
     end
     def create
       text = params[:text]
+      title = params[:title]
       summary = text.slice(0,10)
-      story = Story.create(title:params[:title], summary:summary)
-      page = Story.create(name:title, text:text, story_id:story.id)
+      story = Story.create(title:title, summary:summary)
+      page = Page.create(name:title, text:text, story_id:story.id)
       render json: {storyId:story.id, pageId:page.id}
     end
   end
