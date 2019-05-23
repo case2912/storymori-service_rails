b5c5dd4aebb1a62cdbf21105aa6646aeb24f9b99
case2912 <shiroyama@maia.eonet.ne.jp>
2019-05-23 10:17:18 +0900

    fix /config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace 'api' do
-    resources :stories do
-      resources :pages
+    resources :stories, only:[:index,:create] do
+      resources :pages, only:[:show] do
+      post 'next'
+      end
     end
   end
 end
