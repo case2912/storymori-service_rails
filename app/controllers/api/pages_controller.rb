03efefd3a3e801f28ea76c73a9052adf7128f6eb
case2912 <shiroyama@maia.eonet.ne.jp>
2019-05-23 13:28:59 +0900

    modify show method in pages_controller

--- app/controllers/api/pages_controller.rb
+++ app/controllers/api/pages_controller.rb
@@ -1,6 +1,8 @@
 module Api
   class PagesController < ApplicationController
     def show
+      page = Page.where(story_id:params[:story_id]).find(params[:id])
+      render json: page
     end
     def next
     end
