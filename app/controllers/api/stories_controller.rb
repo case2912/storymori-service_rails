c2983ffb80d3b4afad27f487b1e690e8eca228b1
case2912 <shiroyama@maia.eonet.ne.jp>
2019-05-23 10:36:02 +0900

    modify stories_controller

--- app/controllers/api/stories_controller.rb
+++ app/controllers/api/stories_controller.rb
@@ -3,5 +3,9 @@ class StoriesController < ApplicationController
     render json: Story.all
   end
   def create
+    summary = params[:text].slice(0,10)
+    Story.create(title:params[:title], summary:summary)
+    byebug
+    render json: {}
   end
 end
