34d31ebe930a20b100b671cb6e238e5425cdff8b
case2912 <shiroyama@maia.eonet.ne.jp>
2019-05-24 13:50:03 +0900

    fix next method in pages_controller

--- app/controllers/api/pages_controller.rb
+++ app/controllers/api/pages_controller.rb
@@ -1,21 +1,24 @@
+# frozen_string_literal: true
+
 module Api
   class PagesController < ApplicationController
     def show
       pages = Story.find(params[:story_id]).pages
       page = pages.find(params[:id])
-      children = pages.where(parent_id:params[:id])
+      children = pages.where(parent_id: params[:id])
       res = page.attributes
       res.store('children', children)
       render json: res
     end
+
     def next
-      story_id = params[:story_id]
-      parent_id = params[:page_id]
-      text = params[:text]
-      name = params[:name]
-      page = Page.create(name:name, text:text, story_id:story_id,parent_id:parent_id)
+      page = Page.create(
+        name: params[:name],
+        text: params[:text],
+        story_id: params[:story_id],
+        parent_id: params[:page_id]
+      )
       render json: page
     end
   end
 end
-
