e6abea0d8bf057db2f2cef6d06492d94dba9401c
case2912 <shiroyama@maia.eonet.ne.jp>
2019-05-24 10:39:03 +0900

    format files

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace 'api' do
-    resources :stories, only:[:index,:create] do
-      resources :pages, only:[:show] do
-      post 'next'
+    resources :stories, only: %i[index create] do
+      resources :pages, only: [:show] do
+        post 'next'
       end
     end
   end
